fbc4e960c3780934c84bdb76fd58e9d7e7c3d4be
Yuhee <dbgml4647@naver.com>
2020-07-02 18:22:45 +0900

    1~100까지 더하기

--- 0701.py
+++
@@ -1,7 +0,0 @@
-#10 making a star
-
-a = int(input())
-for i in range(0,a+1):
-    print(" "*(a-i) + "*"*(2*i -1))
- 
-    
---
+++ 0702.py
@@ -0,0 +1,9 @@
+#11 Practice for
+
+s = 0 
+
+for i in range(1,101):
+    s = s + i
+
+
+print(s)
