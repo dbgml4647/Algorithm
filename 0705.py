8c602b694c84058b803c4133067183e876fba5b1
Yuhee <dbgml4647@naver.com>
2020-07-06 18:46:09 +0900

    문자열 입력 연습

--- 0705.py
+++
@@ -1,7 +0,0 @@
-#14 Practice condition
-
-a = int(input())
-if (a % 3) == 0:
-    print("clap")
-else:
-    print(a) 
---
+++ 0706.py
@@ -0,0 +1,4 @@
+#15 Input my name 
+
+a = input()
+print("안녕하세요, 저는"+ a + "입니다")
