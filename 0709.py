8b3621ded0d1372f4647256e256d9c0e8075b2f2
Yuhee <dbgml4647@naver.com>
2020-07-10 18:20:55 +0900

    제곱 구하기

--- 0709.py
+++
@@ -1,8 +0,0 @@
-#18 Practicing repeating 
-
-korean = int(input()) 
-math = int(input())
-english = int(input())
-
-print(int((korean+math+english) / 3))
-
---
+++ 0710.py
@@ -0,0 +1,14 @@
+#19 Practicing 
+
+num_under = int(input())
+num_up = int(input())
+sum = 1
+
+
+for i in range(num_up):
+    sum *= num_under 
+
+
+
+
+print(sum)
