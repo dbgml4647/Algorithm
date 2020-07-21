97d54eba120c7f258569e2ed20bf99029c5e0793
Yuhee <dbgml4647@naver.com>
2020-07-21 14:58:41 +0900

    대 소문자 판단

--- 0719.py
+++
@@ -1,7 +0,0 @@
-#28 2gram
-
-text = input()
-for i in range(len(text)-1):
-    print(text[i],text[i+1], sep="")
-    
-
---
+++ 0721.py
@@ -0,0 +1,7 @@
+#29 Upper
+
+text = input()
+if text.upper() == True:
+    print("OK")
+else:
+    print("NO")
