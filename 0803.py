2dc2b1f55f0f0f9eeba163a046fe413f740261ca
Yuhee <dbgml4647@naver.com>
2020-08-17 15:54:40 +0900

    count 사용하기

--- 0803.py
+++
@@ -1,7 +0,0 @@
-#multifly
-
-num = int(input())
-
-
-for i in range(1,9):
-      print(num*i,end=" ")
---
+++ 0817.py
@@ -0,0 +1,9 @@
+#find name
+
+namelist= input().split(' ')
+
+findname = input()
+
+print(findname,"(이)가 총",namelist.count(findname),"표로 당선!")
+
+#print(namelist[2])
