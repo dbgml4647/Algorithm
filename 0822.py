92de0696a86eeac48f428069c845cf1c77292796
Yuhee <dbgml4647@naver.com>
2020-08-22 17:47:24 +0900

    소수 찾기

---
+++ 0822.py
@@ -0,0 +1,16 @@
+# 41
+
+number = int (input())
+#print(number)
+sum = 0 
+
+for i in range(1,number):
+  if(number % i==0):
+      sum = sum + 1
+
+#print(sum)
+if ( sum < 2):
+  print("YES")
+else:
+  print("NO")
+
