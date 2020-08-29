7f4c4705196000de6f7cec11057a1afaad1e87a8
Yuhee <dbgml4647@naver.com>
2020-08-29 10:56:01 +0900

    str자료형을  이용해 각 자릿수의 합을 구해보자

--- 0828.py
+++
@@ -1,8 +0,0 @@
-#45
-import time
-
-print(time.ctime().split(' ')[-1])
-
-time_now = time.time()
-time_now = int(time_now / (3600 * 24 * 365))+1970
-print(time_now)
---
+++ 0829.py
@@ -0,0 +1,19 @@
+#46
+s = ''
+
+for i in range(101):
+  s += str(i)
+print(s)
+
+sum = 0
+num_list = s
+
+for i in num_list:
+  sum += int(i)
+
+print(sum)
+
+
+#print(num_list[2])
+
+
